5fdf84dbccf4dff6f1933dd6b7da0ab3a1ca7e50
Siny <sinydaniel93@gmail.com>
2022-12-13 18:34:08 -0500

    cart

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -1,5 +1,5 @@
 module "app-module" {
-  source = "https://github.com/Siny93/terraform-mutable-app-deploy.git"
+  source = "git::https://github.com/Siny93/terraform-mutable-app-deploy.git"
   INSTANCE_COUNT = var.INSTANCE_COUNT
   INSTANCE_TYPE = var.INSTANCE_TYPE
   COMPONENT = var.COMPONENT
